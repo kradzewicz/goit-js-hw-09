53c340710b13e3228e7e17c37bdec7eec6b8ad6a
kradzewicz <124262553+kradzewicz@users.noreply.github.com>
2024-03-02 02:35:09 +0100

    first commit

---
+++ src/js/02-timer.js
@@ -0,0 +1,81 @@
+import flatpickr from 'flatpickr';
+import 'flatpickr/dist/flatpickr.min.css';
+
+const timePicker = document.querySelector('#datetime-picker');
+const startBtn = document.querySelector('[data-start]');
+const days = document.querySelector('[data-days]');
+const hours = document.querySelector('[data-hours]');
+const minutes = document.querySelector('[data-minutes]');
+const seconds = document.querySelector('[data-seconds]');
+
+startBtn.disabled = true;
+
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    if (selectedDates[0].getTime() <= options.defaultDate.getTime()) {
+      window.alert('Please choose date in future!');
+      startBtn.disabled = true;
+    } else {
+      startBtn.disabled = false;
+    }
+  },
+};
+
+const flatPicker = flatpickr(timePicker, options);
+
+function convertMs(ms) {
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  const days = Math.floor(ms / day);
+  const hours = Math.floor((ms % day) / hour);
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}
+
+function addLeadingZero(value) {
+  value.days = String(value.days).padStart(2, '0');
+  value.hours = String(value.hours).padStart(2, '0');
+  value.minutes = String(value.minutes).padStart(2, '0');
+  value.seconds = String(value.seconds).padStart(2, '0');
+}
+
+function startCounting() {
+  startBtn.disabled = true;
+  const futureDate = flatPicker.selectedDates[0].getTime();
+
+  function countingValue() {
+    const todayDate = new Date().getTime();
+    const timeDiffer = futureDate - todayDate;
+    const dateDiffer = convertMs(timeDiffer);
+
+    addLeadingZero(dateDiffer);
+
+    days.textContent = dateDiffer.days;
+    hours.textContent = dateDiffer.hours;
+    minutes.textContent = dateDiffer.minutes;
+    seconds.textContent = dateDiffer.seconds;
+
+    if (timeDiffer <= 0) {
+      days.textContent = '00';
+      hours.textContent = '00';
+      minutes.textContent = '00';
+      seconds.textContent = '00';
+      clearInterval(counting);
+    }
+  }
+
+  const counting = setInterval(countingValue, 1000);
+
+  countingValue();
+}
+
+startBtn.addEventListener('click', startCounting);
--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,44 @@
+const form = document.querySelector('.form');
+const firstDelay = document.querySelector('[name="delay"]');
+const step = document.querySelector('[name="step"]');
+const amount = document.querySelector('[name="amount"]');
+const inputs = document.querySelectorAll('[type="number"]');
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  const promise = new Promise((resolve, reject) => {
+    if (shouldResolve) {
+      resolve(`✅ Fulfilled promise ${position} in ${delay}ms`);
+    } else {
+      reject(`❌ Rejected promise ${position} in ${delay}ms`);
+    }
+  });
+  return promise;
 }
+
+function promiseCreator(event) {
+  event.preventDefault();
+
+  const positions = [];
+  for (let i = 1; i <= amount.value; i++) {
+    positions.push(i);
+  }
+
+  setTimeout(() => {
+    positions.forEach((position, index) => {
+      setTimeout(() => {
+        const delay = parseInt(firstDelay.value) + parseInt(step.value * index);
+        createPromise(position, delay)
+          .then(resolved => console.log(resolved))
+          .catch(rejected => console.log(rejected));
+        if (positions.length === position) {
+          inputs.forEach(input => {
+            input.value = '';
+          });
+        }
+      }, step.value);
+    });
+  }, firstDelay.value);
+}
+
+form.addEventListener('submit', promiseCreator);
