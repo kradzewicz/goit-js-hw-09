1a4d34894c7099247fa078388eb57f44c7508569
kradzewicz <124262553+kradzewicz@users.noreply.github.com>
2024-03-02 03:54:13 +0100

    01-color-switcher update

--- src/js/01-color-switcherrr.js
+++ src/js/01-color-switcherrr.js
@@ -4,15 +4,19 @@ const stopBtn = document.querySelector('[data-stop]');
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+startBtn.disabled = false;
+stopBtn.disabled = true;
 
 startBtn.addEventListener('click', () => {
   timerId = setInterval(() => {
     document.body.style.backgroundColor = getRandomHexColor();
   }, 1000);
-  startBtn.disabled = true;
+  startBtn.toggleAttribute('disabled');
+  stopBtn.toggleAttribute('disabled');
 });
 
 stopBtn.addEventListener('click', () => {
   clearInterval(timerId);
-  startBtn.disabled = false;
+  startBtn.toggleAttribute('disabled');
+  stopBtn.toggleAttribute('disabled');
 });
